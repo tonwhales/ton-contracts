a06aa19ddc7eb1f0db20224b61f5be0575f8e83b
Steve Korshakov <steve@korshakov.com>
2021-10-01 15:11:32 +0300

    wip: working compilation

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
-./node_modules/ton-compiler/bin/macos/func -PS -o ./contracts/wallet.fift $(pwd)/stdlib.fc ./contracts/wallet.func
+./node_modules/ton-compiler/bin/macos/func -PS -A -o ./contracts/wallet.fif $(pwd)/stdlib.fc ./contracts/wallet.func
+export FIFTPATH=$(pwd)/node_modules/ton-compiler/fiftlib/
+./node_modules/ton-compiler/bin/macos/fift build.fif
