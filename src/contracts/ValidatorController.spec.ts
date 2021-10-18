4f6de7a47b190a6bd08b7a480f70d3671e0d7175
Steve Korshakov <steve@korshakov.com>
2021-10-18 15:54:17 +0300

    feat: fix big payload handling

--- src/contracts/ValidatorController.spec.ts
+++ src/contracts/ValidatorController.spec.ts
@@ -90,6 +90,41 @@ describe('ValidatorController', () => {
         expect(cooldown).toBeGreaterThan(0);
     }, 120000);
 
+    it('should transfer via master key with big payload', async () => {
+        let state = await createNewWallets();
+
+        // Transfer to whitelisted to init
+        expect(await state.wallet.getSeqNo()).toBe(0);
+        await state.wallet.transfer({
+            seqno: 0,
+            to: state.foreignWallet.wallet.address,
+            value: toNano(0.001),
+            secretKey: state.masterKey.secretKey,
+            bounce: false,
+            payload: Buffer.from([
+                0x52, 0x67, 0x43, 0x70,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+            ])
+        });
+        await awaitBalance(client, state.foreignWallet.wallet.address, new BN(0));
+
+        // Check balances
+        expect((await client.getBalance(state.wallet.address)).toNumber()).toBeGreaterThan(0);
+        expect((await client.getBalance(state.foreignWallet.wallet.address)).toNumber()).toBeGreaterThan(0);
+
+        // Check cooldown
+        let cooldown = parseInt((await client.callGetMethod(state.wallet.address, 'restricted_cooldown')).stack[0][1], 16);
+        expect(cooldown).toBeGreaterThan(0);
+    }, 120000);
+
     it('should transfer via restricted key to whitelisted', async () => {
         let state = await createNewWallets();
         expect(await state.wallet.getSeqNo()).toBe(0);
@@ -159,110 +194,4 @@ describe('ValidatorController', () => {
         });
         await expect(awaitSeqno(state.wallet, 1)).rejects.toThrowError();
     }, 120000);
-
-    // it('should work', async () => {
-
-
-
-    //     // Whitelisted wallet key
-    //     let whitelistedWallet = await client.createNewWallet({ workchain: 0 });
-
-    //     // Wallet that is allowed only for non-restricted key
-    //     let foreignWallet = await client.createNewWallet({ workchain: 0 });
-
-    //     // Wallet Keys
-    //     let masterKey = await createWalletKey();
-    //     let invalidKey = await createWalletKey();
-    //     let restrictedKey = await createWalletKey();
-
-    //     // Wallet
-    //     const wallet = await client.openWalletFromCustomContract(ValidatorControllerSource.create({
-    //         masterKey: masterKey.publicKey,
-    //         restrictedKey: restrictedKey.publicKey,
-    //         workchain: 0,
-    //         whitelistedAddress: whitelistedWallet.wallet.address
-    //     }));
-
-    //     // Requirements
-    //     expect((await client.getBalance(wallet.address)).toNumber()).toBe(0);
-    //     expect((await client.getBalance(whitelistedWallet.wallet.address)).toNumber()).toBe(0);
-
-    //     // Fill wallet
-    //     await topUpAddress(client, wallet.address, toNano(0.1));
-
-    //     // Send transfer via master key
-    //     expect(await wallet.getSeqNo()).toBe(0);
-    //     await wallet.transfer({
-    //         seqno: 0,
-    //         to: foreignWallet.wallet.address,
-    //         value: toNano(0.001),
-    //         secretKey: masterKey.secretKey,
-    //         bounce: false
-    //     });
-    //     await awaitBalance(client, foreignWallet.wallet.address, new BN(0));
-    //     let cooldown = parseInt((await client.callGetMethod(wallet.address, 'restricted_cooldown')).stack[0][1], 16);
-    //     expect(cooldown).toBeGreaterThan(0);
-
-    //     // Send transfer via invalid key
-    //     expect(await wallet.getSeqNo()).toBe(1);
-    //     let initial = await client.getBalance(wallet.address);
-    //     await wallet.transfer({
-    //         seqno: 1,
-    //         to: whitelistedWallet.wallet.address,
-    //         value: toNano(0.002),
-    //         secretKey: invalidKey.secretKey,
-    //         bounce: false
-    //     });
-    //     await delay(15000);
-    //     expect((await client.getBalance(wallet.address)).eq(initial)).toBe(true);
-    //     cooldown = parseInt((await client.callGetMethod(wallet.address, 'restricted_cooldown')).stack[0][1], 16);
-    //     expect(cooldown).toBe(0);
-
-    //     // Send transfer via restireted key to non-whitelisted
-    //     expect(await wallet.getSeqNo()).toBe(1);
-    //     initial = await client.getBalance(wallet.address);
-    //     await wallet.transfer({
-    //         seqno: 1,
-    //         to: foreignWallet.wallet.address,
-    //         value: toNano(0.003),
-    //         secretKey: restrictedKey.secretKey,
-    //         bounce: false
-    //     });
-    //     await delay(15000);
-    //     expect((await client.getBalance(wallet.address)).eq(initial)).toBe(true);
-    //     cooldown = parseInt((await client.callGetMethod(wallet.address, 'restricted_cooldown')).stack[0][1], 16);
-    //     expect(cooldown).toBe(0);
-
-    //     // Result seqno
-    //     expect(await wallet.getSeqNo()).toBe(1);
-
-    //     // Send transfer via restricted key
-    //     expect(await wallet.getSeqNo()).toBe(1);
-    //     await wallet.transfer({
-    //         seqno: 1,
-    //         to: whitelistedWallet.wallet.address,
-    //         value: toNano(0.004),
-    //         secretKey: restrictedKey.secretKey,
-    //         bounce: false
-    //     });
-    //     await awaitBalance(client, whitelistedWallet.wallet.address, new BN(0));
-    //     cooldown = parseInt((await client.callGetMethod(wallet.address, 'restricted_cooldown')).stack[0][1], 16);
-    //     await delay(5000); // Race condition?
-    //     expect(cooldown).toBeGreaterThan(0);
-
-    //     // If sent too often it must be ignored
-    //     initial = await client.getBalance(whitelistedWallet.wallet.address);
-    //     expect(await wallet.getSeqNo()).toBe(2);
-    //     await wallet.transfer({
-    //         seqno: 2,
-    //         to: whitelistedWallet.wallet.address,
-    //         value: toNano(0.005),
-    //         secretKey: restrictedKey.secretKey,
-    //         bounce: false
-    //     });
-    //     await delay(15000);
-    //     expect((await client.getBalance(whitelistedWallet.wallet.address)).eq(initial)).toBe(true);
-    //     cooldown = parseInt((await client.callGetMethod(wallet.address, 'restricted_cooldown')).stack[0][1], 16);
-    //     expect(cooldown).toBeGreaterThan(0);
-    // }, 120000);
 });
