c61ba712146140499303ae707b2b56cea969bc83
Steve Korshakov <steve@korshakov.com>
2021-11-04 00:06:47 +0300

    fix: fix missing exports of logger and elector contracts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { WhitelistedWalletSource } from './contracts/WhitelistedWalletSource';
 export { ValidatorControllerSource } from './contracts/ValidatorControllerSource';
 export { PowGiverContract } from './contracts/PowGiverContract';
+export { LoggerContract } from './contracts/LoggerContract';
+export { ElectorContract } from './contracts/ElectorContract';
