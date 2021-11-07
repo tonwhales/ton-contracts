1cdd8d298e7ca86c91367513dd23297cac413fd6
Steve Korshakov <steve@korshakov.com>
2021-11-07 14:23:28 +0300

    feat: implement all features of PowGiver

--- src/contracts/PowGiverContract.spec.ts
+++ src/contracts/PowGiverContract.spec.ts
@@ -14,4 +14,20 @@ describe('PowGiverContract', () => {
         expect(powParams2.seed.equals(powParams.seed)).toBe(true);
         expect(powParams2.complexity.equals(powParams.complexity)).toBe(true);
     });
+    it('should create mining message', async () => {
+        let goldenMessage = Buffer.from('te6cckEBAgEAoQABRYn+60cGmp15E/BYb2BjW+nJxNPzCJuqiqGX5SoOin0HYkQMAQDyTWluZQBhh6Dsg9/VUuY3KbRy/LzIxF68xmkXAlWLaOx1J+G6QDoPMaiE5Zx20ujtiB/iVncNL1B+7Bkm+Snqt135M09CtadvM5Av3y3C82L1CWZeoeat+DWE5Zx20ujtiB/iVncNL1B+7Bkm+Snqt135M09CtadvMzU5dJw=', 'base64');
+        let goldenRandom = Buffer.from('hOWcdtLo7Ygf4lZ3DS9QfuwZJvkp6rdd+TNPQrWnbzM=', 'base64');
+        let goldenSeed = Buffer.from('kC/fLcLzYvUJZl6h5q34NQ==', 'base64');
+        let msg = PowGiverContract.createMiningMessage({
+            giver: Address.parse('Ef91o4NNTryJ-Cw3sDGt9OTiafmETdVFUMvylQdFPoOxInls'),
+            wallet: Address.parse('EQCD39VS5jcptHL8vMjEXrzGaRcCVYto7HUn4bpAOg8xqB2N'),
+            seed: goldenSeed,
+            random: goldenRandom,
+            expiresSec: 1636278508
+        });
+        let msgCell = new Cell();
+        msg.writeTo(msgCell);
+        let msgData = await msgCell.toBoc({ idx: false });
+        expect(msgData).toEqual(goldenMessage)
+    });
 });
--- src/contracts/PowGiverContract.ts
+++ src/contracts/PowGiverContract.ts
@@ -1,5 +1,5 @@
-import { BN } from "bn.js";
-import { Address, Contract, UnknownContractSource, TonClient, Cell, BitStringReader, BitString } from "ton";
+import { Address, Contract, UnknownContractSource, TonClient, Cell, BitStringReader, BitString, ExternalMessage, CommonMessageInfo, RawMessage, BinaryMessage } from "ton";
+import { sha256 } from "ton-crypto";
 import { createUInt32 } from "./utils/createUInt32";
 
 
@@ -30,6 +30,12 @@ export class PowGiverContract implements Contract {
         return new PowGiverContract(address, client);
     }
 
+    /**
+     * Extracts pow params from contract state without need to invoke get pow_params. This is faster, more predictable and allows to handle
+     * race conditions when poling from multiple sources
+     * @param cell state cell
+     * @returns seed and complexity
+     */
     static async extractPowParamsFromState(cell: Cell) {
 
         // Reimplementation
@@ -45,7 +51,13 @@ export class PowGiverContract implements Contract {
         }
     }
 
-    static async createMiningJob(seed: Buffer, random: Buffer, wallet: Address, expires: number, bounce: boolean) {
+    /**
+     * Creates header of mining job. Just apply random, seed and random again to make a full job.
+     * @param wallet wallet to mine to
+     * @param expiresSec job expiration unixtime in seconds
+     * @returns Buffer of job header
+     */
+    static createMiningJobHeader(wallet: Address, expiresSec: number) {
 
         //
         // https://github.com/ton-blockchain/ton/blob/15dfedd371f1dfc4502ab53c6ed99deb1922ab1a/crypto/util/Miner.cpp#L57
@@ -54,13 +66,103 @@ export class PowGiverContract implements Contract {
         return Buffer.concat([
             Buffer.from([0x0, 0xFA]), // Important prefix: https://github.com/ton-blockchain/ton/blob/15dfedd371f1dfc4502ab53c6ed99deb1922ab1a/crypto/util/Miner.cpp#L50
             Buffer.from('Mine'), // Op
-            Buffer.from([wallet.workChain * 4 + (bounce ? 1 : 0)]), // Wallet key
-            createUInt32(expires), // Expire
-            wallet.hash,
-            random,
-            seed,
-            random
+            Buffer.from([0]), // Workchain + Bounce. Set them all to zero.
+            createUInt32(expiresSec), // Expire in seconds
+            wallet.hash // Wallet hash
         ]);
+    }
+
+    /**
+     * Creates full mining job
+     * @param args.seed giver's current seed
+     * @param args.random random value
+     * @param args.wallet wallt to mine to
+     * @param args.expiresSec job expiration unixtime in seconds
+     * @returns Buffer of job
+     */
+    static createMiningJob(args: { seed: Buffer, random: Buffer, wallet: Address, expiresSec: number }) {
+
+        //
+        // Check address
+        // 
+
+        if (args.wallet.workChain !== 0) {
+            throw Error('Only walelts in basic workchain are supported');
+        }
+
+        //
+        // https://github.com/ton-blockchain/ton/blob/15dfedd371f1dfc4502ab53c6ed99deb1922ab1a/crypto/util/Miner.cpp#L57
+        //
+
+        return Buffer.concat([
+            PowGiverContract.createMiningJobHeader(args.wallet, args.expiresSec),
+            args.random, // Random
+            args.seed, // Seed
+            args.random // Random
+        ]);
+    }
+
+    /**
+     * Checks if mining result is valid
+     * @param args.seed giver's current seed
+     * @param args.random random value
+     * @param args.wallet wallt to mine to
+     * @param args.expiresSec job expiration unixtime in seconds
+     * @param args.hash computed hash
+     * @returns 
+     */
+    static async checkMiningJobHash(args: { seed: Buffer, random: Buffer, wallet: Address, expiresSec: number, hash: Buffer }) {
+        const job = PowGiverContract.createMiningJob({ seed: args.seed, random: args.random, wallet: args.wallet, expiresSec: args.expiresSec });
+        const hash = await sha256(job);
+        return args.hash.equals(hash);
+    }
+
+    /**
+     * Creates mining message to send to giver
+     * @param args.giver giver address 
+     * @param args.seed giver seed
+     * @param args.random giver random
+     * @param args.wallet wallet to mine to
+     * @param args.expiresSec expires in seconds
+     * @returns 
+     */
+    static createMiningMessage(args: { giver: Address, seed: Buffer, random: Buffer, wallet: Address, expiresSec: number }) {
+
+        //
+        // Check address
+        // 
+
+        if (args.wallet.workChain !== 0) {
+            throw Error('Only walelts in basic workchain are supported');
+        }
+
+        //
+        // Message body
+        //
+
+        const body = Buffer.concat([
+            // Note that 0x00FA are not in the message, but it is a part of a hashing job
+            // Buffer.from([0x0, 0xFA]), // Important prefix: https://github.com/ton-blockchain/ton/blob/15dfedd371f1dfc4502ab53c6ed99deb1922ab1a/crypto/util/Miner.cpp#L50
+            Buffer.from('Mine'), // Op
+            Buffer.from([0]), // Workchain * 4 + Bounce. Set them all to zero.
+            createUInt32(args.expiresSec), // Expire in seconds
+            args.wallet.hash, // Wallet hash,
+            args.random, // Random
+            args.seed, // Seed
+            args.random // Random
+        ]);
+
+        //
+        // External message
+        //
+        const externalMessage = new ExternalMessage({
+            to: args.giver,
+            body: new CommonMessageInfo({
+                body: new BinaryMessage(body)
+            })
+        });
+
+        return externalMessage;
     }
 
     readonly client: TonClient;
